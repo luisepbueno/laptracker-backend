0d56fa65d497f0b0fe8cef03c4c595ac4633a8af
Luis <luisepbueno@gmail.com>
2021-09-07 00:23:56 +0000

    Do not use dotenv in production environment
    Signed-off-by: Luis <luisepbueno@gmail.com>

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const sequelize = require('./utils/database');
 
 const isAuth = require('./middleware/auth');
--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
 const DB_NAME = process.env.DB_NAME;
